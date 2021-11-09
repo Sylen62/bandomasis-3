7e15cabc4c55dc11ae1f35d25d4722f5eba29979
Justas Peldžius <peldjustas@gmail.com>
2021-11-09 14:12:45 +0200

    6: added method to get cars in CarGridComponent

--- js/components/car-grid-component.js
+++ js/components/car-grid-component.js
@@ -6,8 +6,28 @@ class CarGridComponent {
 		this.init();
 	}
 
+	showError = (error) => alert(error);
+
+	saveCars = (cars) => {
+		this.state = { cars };
+		this.render();
+	};
+
+	getCars = () => API.fetchCars(this.saveCars, this.showError);
+
 	init = () => {
+		this.getCars();
 		this.htmlElement = document.createElement('div');
 		this.htmlElement.innerHTML = `<h1>CarGridComponent</h1>`;
+		this.render();
+	};
+
+	render = () => {
+		const { cars } = this.state;
+		if (cars.length === 0) {
+			this.htmlElement.innerHTML = `<h1>Siunčiama...</h1>`;
+		} else if (cars.length > 0) {
+			this.htmlElement.innerHTML = `<h1>Parsiųsta!</h1>`;
+		}
 	};
 }
